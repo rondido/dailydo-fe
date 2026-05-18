1998cdb280254e01f98a4dd8515c1284466956ca
bluemistmark <bluemistmark@gmail.com>
2026-05-18 19:37:45 +0900

    feat: 하드코딩 라우트 상수로 교체

--- src/shared/api/fetchClient.ts
+++ src/shared/api/fetchClient.ts
@@ -1,3 +1,5 @@
+import { ROUTES } from '@/shared/config/routes';
+
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 if (!BASE_URL) throw new Error('NEXT_PUBLIC_API_URL을 찾을 수 없습니다.');
@@ -17,7 +19,7 @@ export const fetchClient = async <T = unknown>(
 
   if (res.status === 401) {
     if (typeof window !== 'undefined') {
-      window.location.href = '/login'
+      window.location.href = ROUTES.LOGIN;
     }
     throw new Error('Unauthorized');
   }
@@ -27,9 +29,9 @@ export const fetchClient = async <T = unknown>(
   }
 
   if (!res.ok) {
-    const error = await res.json().catch(() => ({}))
+    const error = await res.json().catch(() => ({}));
     throw new Error(error?.message ?? 'API ERROR가 발생했습니다.');
   }
 
   return res.json() as Promise<T>;
-}
+};
