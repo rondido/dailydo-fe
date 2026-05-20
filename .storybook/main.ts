b5c474ee91203b64fa0250087b15a9951f8f652a
sohxxny <sohxxny@gmail.com>
2026-05-20 15:49:50 +0900

    chore: 스토리북에 커스텀 폰트 적용 (#11)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,6 +4,6 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-a11y', '@storybook/addon-docs'],
   framework: '@storybook/nextjs',
-  staticDirs: ['../public'],
+  staticDirs: ['../public', { from: '../src/app/fonts', to: '/fonts' }],
 };
 export default config;
