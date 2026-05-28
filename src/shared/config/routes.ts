4f76a3e55b5028d72d4b275efb302f15a668d705
sohxxny <sohxxny@gmail.com>
2026-05-28 17:56:50 +0900

    chore: 라우팅 및 공통 설정 변경 (#34)

--- src/shared/config/routes.ts
+++ src/shared/config/routes.ts
@@ -3,5 +3,19 @@ export const ROUTES = {
   LOGIN: '/login',
   SIGNUP: '/signup',
   MISSIONS: '/missions',
+  MYPAGE: '/mypage',
+  MYLOG: '/mylogs',
+  COLLECTIONS: '/collections',
   AUTH_CALLBACK: '/auth/callback',
 } as const;
+
+export const ROUTES_NAME: Record<keyof typeof ROUTES, string> = {
+  HOME: '홈',
+  LOGIN: '로그인',
+  SIGNUP: '회원가입',
+  MISSIONS: '오늘의 미션',
+  MYPAGE: '마이페이지',
+  MYLOG: '마이로그',
+  COLLECTIONS: '마이컬렉션',
+  AUTH_CALLBACK: '로그인 콜백',
+};
