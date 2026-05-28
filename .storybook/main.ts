376006758798c1ed2b647e01f08ca100b89ed1b0
sohxxny <sohxxny@gmail.com>
2026-05-28 16:16:12 +0900

    chore: storybook main.ts에 svgr 설정 추가 (#41)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,5 +5,32 @@ const config: StorybookConfig = {
   addons: ['@storybook/addon-a11y', '@storybook/addon-docs'],
   framework: '@storybook/nextjs',
   staticDirs: ['../public', { from: '../src/app/fonts', to: '/fonts' }],
+
+  webpackFinal: async (config) => {
+    config.module = config.module || {};
+    config.module.rules = config.module.rules || [];
+
+    const imageRule = config.module.rules.find(
+      (rule) =>
+        rule !== null &&
+        typeof rule === 'object' &&
+        (rule as { test?: RegExp }).test?.test('.svg'),
+    );
+    if (imageRule) {
+      (imageRule as { exclude?: RegExp }).exclude = /\.svg$/;
+    }
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: { svgoConfig: { plugins: ['removeDimensions'] } },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 export default config;
