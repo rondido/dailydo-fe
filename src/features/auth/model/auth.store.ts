e60f4391b9a21808d5272e74992e6aeaee7fc240
bluemistmark <bluemistmark@gmail.com>
2026-06-01 10:17:19 +0900

    fix: 로컬 스토리지 저장 토큰 삭제

--- src/features/auth/model/auth.store.ts
+++ src/features/auth/model/auth.store.ts
@@ -7,23 +7,14 @@ const AUTH_STORE_KEY = 'dailydo_auth';
 
 interface AuthState {
   lastLogin: SocialLoginType | null;
-  accessToken: string | null;
-  refreshToken: string | null;
   setLastLogin: (type: SocialLoginType) => void;
-  setAuth: (accessToken: string, refreshToken: string) => void;
-  clearAuth: () => void;
 }
 
 export const useAuthStore = create<AuthState>()(
   persist(
     (set) => ({
       lastLogin: null,
-      accessToken: null,
-      refreshToken: null,
       setLastLogin: (type) => set({ lastLogin: type }),
-      setAuth: (accessToken, refreshToken) =>
-        set({ accessToken, refreshToken }),
-      clearAuth: () => set({ accessToken: null, refreshToken: null }),
     }),
     {
       name: AUTH_STORE_KEY,
