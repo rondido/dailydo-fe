f4adefd27036d658784a93f878ede3d1c5d16694
bluemistmark <bluemistmark@gmail.com>
2026-06-02 13:02:47 +0900

    feat: index 페이지 추가

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,5 @@
+import { SignupPage } from '@/views/signup';
+
+export default function Page() {
+  return <SignupPage />;
+}
---
+++ src/features/signup/index.ts
@@ -0,0 +1,3 @@
+export { CategoryStep } from './ui/category-step';
+export { NicknameStep } from './ui/nickname-step';
+export { WelcomeStep } from './ui/welcome-step';
