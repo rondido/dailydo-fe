66370830b77aa4d4111a85105914050420da079e
bluemistmark <bluemistmark@gmail.com>
2026-06-04 15:34:08 +0900

    fix: 미사용 파일 제거

--- src/entities/category/api/category-api.ts
+++
@@ -1,7 +0,0 @@
-import { serverApi } from '@/shared/api/fetch-server';
-
-import { Category } from '../model/types';
-
-export const getCategories = (): Promise<Category[]> => {
-  return serverApi.get<Category[]>('/api/category');
-};
