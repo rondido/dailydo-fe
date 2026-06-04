469dbcefd4317aa347b70271417f3802b8739720
sohxxny <sohxxny@gmail.com>
2026-06-04 19:53:05 +0900

    design: 모바일 화면 사이드 배경 색상 변경

--- src/app/(mobile)/layout.tsx
+++ src/app/(mobile)/layout.tsx
@@ -1,7 +1,13 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="mx-auto flex h-full max-w-107.5 min-w-90 flex-col">
-      {children}
+    <div className="h-full w-full bg-gray-50">
+      <div className="mx-auto flex h-full max-w-107.5 min-w-90 flex-col">
+        {children}
+        <div
+          id="mobile-portal-root"
+          className="pointer-events-none fixed inset-y-0 left-1/2 z-200 w-full max-w-107.5 min-w-90 -translate-x-1/2 overflow-hidden"
+        />
+      </div>
     </div>
   );
 }
