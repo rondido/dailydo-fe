9460ea3e29274294f8d9f5bff90a62f15477f08e
bluemistmark <bluemistmark@gmail.com>
2026-06-22 16:47:33 +0900

    refactor: 폰트 용량 최적화

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,33 @@ export const metadata: Metadata = {
 };
 
 const pretendard = localFont({
-  src: './fonts/PretendardVariable.woff2',
+  src: [
+    {
+      path: './fonts/Pretendard-Light.subset.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Pretendard-Regular.subset.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Pretendard-Medium.subset.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Pretendard-SemiBold.subset.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Pretendard-Bold.subset.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
   variable: '--font-pretendard',
 });
 
